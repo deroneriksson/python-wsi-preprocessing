bda8499b3b898b5f6d12caa3b5180a3b6f433b14
Deron Eriksson <deron@apache.org>
2018-01-15 16:48:03 -0800

    Remove preceding whitespace from csv columns

--- wsi/tile.py
+++ wsi/tile.py
@@ -307,8 +307,8 @@ def save_tile_data(tile_summary):
   """
   csv = summary_text(tile_summary)
 
-  csv += "\n\n\nTile Num,Row,Column,Tissue %,Col Start, Row Start, Col End, Row End, Col Size, Row Size," + \
-         "Original Col Start, Original Row Start, Original Col End, Original Row End, Original Col Size, Original Row Size\n"
+  csv += "\n\n\nTile Num,Row,Column,Tissue %,Col Start,Row Start,Col End,Row End,Col Size,Row Size," + \
+         "Original Col Start,Original Row Start,Original Col End,Original Row End,Original Col Size,Original Row Size\n"
 
   count = 0
   for t in tile_summary.tiles:
